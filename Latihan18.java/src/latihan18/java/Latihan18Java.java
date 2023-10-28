a6fc13a2546ac4b56964ad6f6dc0c6e4e92e8719
Evanlaksana <132762464+Evanlaksana@users.noreply.github.com>
2023-10-28 17:13:13 +0700

    Add files via upload

---
+++ Latihan18.java/src/latihan18/java/Latihan18Java.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package latihan18.java;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Latihan18Java {
+
+    public static void main(String[] args) {
+        
+        for (double i = 3.5; i <= 35; i += 3.5) {
+            System.out.println(i);
+        }
+    }
+}
+    
+    
